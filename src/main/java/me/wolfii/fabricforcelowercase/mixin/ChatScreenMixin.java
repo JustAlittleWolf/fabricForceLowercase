e8242fe38e7477e7db029d123ef64f132a3d77e7
JustAlittleWolf <54244277+JustAlittleWolf@users.noreply.github.com>
2023-04-22 13:50:38 +0200

    Update mod Icon

--- src/main/java/me/wolfii/fabricforcelowercase/mixin/ChatScreenMixin.java
+++ src/main/java/me/wolfii/fabricforcelowercase/mixin/ChatScreenMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(ChatScreen.class)
 public class ChatScreenMixin {
     @ModifyArg(method = { "keyPressed" }, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ChatScreen;sendMessage(Ljava/lang/String;Z)Z"), index = 0)
-    private String result(final String text) {
+    private String result(String text) {
         if (text.startsWith("/")) {
             return StringHelper.firstArgumentToLowerCase(text);
         }
--- src/main/java/me/wolfii/fabricforcelowercase/mixin/SleepingChatScreenMixin.java
+++ src/main/java/me/wolfii/fabricforcelowercase/mixin/SleepingChatScreenMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(SleepingChatScreen.class)
 public class SleepingChatScreenMixin {
     @ModifyArg(method = { "keyPressed" }, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/SleepingChatScreen;sendMessage(Ljava/lang/String;Z)Z"), index = 0)
-    private String result(final String text) {
+    private String result(String text) {
         if (text.startsWith("/")) {
             return StringHelper.firstArgumentToLowerCase(text);
         }
