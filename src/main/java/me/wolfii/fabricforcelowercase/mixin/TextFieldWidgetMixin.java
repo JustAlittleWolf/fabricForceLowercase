a2a4d44b7d3bf3635b37c9a55d81ee562b38203c
JustAlittleWolf <54244277+JustAlittleWolf@users.noreply.github.com>
2023-05-01 12:09:05 +0200

    Improve text modification

--- src/main/java/me/wolfii/fabricforcelowercase/mixin/TextFieldWidgetMixin.java
+++ src/main/java/me/wolfii/fabricforcelowercase/mixin/TextFieldWidgetMixin.java
@@ -6,18 +6,28 @@ import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TextFieldWidget.class)
 public class TextFieldWidgetMixin {
     @Shadow
     private String text;
 
-    @Redirect(method = "write", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;text:Ljava/lang/String;", opcode = Opcodes.PUTFIELD))
-    private void modifiedWriteInput(TextFieldWidget textFieldWidget, String text) {
-        if (text.startsWith("/")) {
-            text =  StringHelper.firstArgumentToLowerCase(text);
+    @Inject(method = "getText", at = @At("HEAD"), cancellable = true)
+    private void modifiedGetText(CallbackInfoReturnable<String> cir) {
+        if (this.text.startsWith("/")) {
+            cir.setReturnValue(StringHelper.firstArgumentToLowerCase(this.text));
         }
-        this.text = text;
+    }
+
+    @Redirect(method = "renderButton", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;text:Ljava/lang/String;", opcode = Opcodes.GETFIELD))
+    private String modifiedGetText(TextFieldWidget instance) {
+        String finalText = this.text;
+        if (this.text.startsWith("/")) {
+            finalText = StringHelper.firstArgumentToLowerCase(text);
+        }
+        return finalText;
     }
 }
